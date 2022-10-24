840aa94ea9d3219b4ecead3e31c4d8889104dc45
Tomasz Wiercioch <47215529+tomawash@users.noreply.github.com>
2022-10-24 12:37:42 -0400

    Added Jumping

--- Tomasz_MTEC2210_ToCatchATheme/Assets/Scripts/PlayerController.cs
+++ Tomasz_MTEC2210_ToCatchATheme/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,13 @@ public class PlayerController : MonoBehaviour
     public AudioClip hazardClip;
     //public AudioSource audioSource;
     public GameManager gm;
-    public float speed = 5;
+    public float speed = 200;
+    public float jumpSpeed = 10;
+
+    private Rigidbody2D rb;
+
+    bool jumping;
+
     public float health = 10;
 
     public float oldpos = 0;
@@ -20,19 +26,45 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxis("Horizontal");
+        //float xMove = Input.GetAxis("Horizontal");
 
         //Debug.Log("xMove: " + xMove);
-        transform.Translate(xMove * speed * Time.deltaTime, 0, 0);
+        //transform.Translate(xMove * speed * Time.deltaTime, 0, 0);
 
-        //transform.position = new Vector3(0, 0, 0);
 
+
+        //Method 1 - Translation Method
+        /*
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+        */
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+        }
+
+        //Flip sprite
         float temp = gameObject.transform.position.x;
 
         if (temp > oldpos)
@@ -44,6 +76,26 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
         oldpos = temp;
+    }
+
+    public void FixedUpdate()
+    {
+        //Method 2 - Velocity method
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed * Time.deltaTime, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed * Time.deltaTime, rb.velocity.y);
+        }
+
+        if(jumping == true)
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+            jumping = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
